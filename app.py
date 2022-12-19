bccfed5f23ad8f875f89511e86f534af1fb05bf8
Алексей <stormwww@yandex.ru>
2022-12-19 16:00:01 +0300

    test

--- app.py
+++ app.py
@@ -1,18 +1,44 @@
 import fastapi
+import pydantic_models
 
 
 api = fastapi.FastAPI()
 
-response = {"Ответ": "Который возвращает сервер"}
+fake_database = {'users':[
 
-@api.get('/')
-def index():
-    return response
+    {
+        "id":1,
+        "name":"Anna",
+        "nick":"Anny42",
+        "balance": 15300
+     },
 
-@api.get('/hello')
-def hello():
-    return "hello"
+    {
+        "id":2,
+        "name":"Dima",
+        "nick":"dimon2319",
+        "balance": 160.23
+     }
+    ,{
+        "id":3,
+        "name":"Vladimir",
+        "nick":"Vova777",
+        "balance": "25000"
+     }
+],}
 
-@api.get('/about/us')
-def about():
-    return {"we are": "legion"}
+@api.get('/get_info_by_user_id/{id:int}')
+def get_info_about_user(id):
+    return fake_database['users'][id-1]
+
+@api.get('/get_user_balance_by_id/{id:int}')
+def get_user_balance(id):
+    return fake_database['users'][id-1]['balance']
+
+
+@api.get('/get_total_balance')
+def get_total_balance():
+    total_balance: float = 0.0
+    for user in fake_database['users']:
+        total_balance += pydantic_models.User(**user).balance
+    return total_balance
